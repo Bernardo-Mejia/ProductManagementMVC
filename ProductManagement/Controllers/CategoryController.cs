5a1740205a1062ad743b076631048b6ab46dac36
Bernardo Gabriel Mejía Pérez <bernardo.mejiaperez703@gmail.com>
2023-09-17 12:07:38 -0600

    Edit and delete actions

--- ProductManagement/Controllers/CategoryController.cs
+++ ProductManagement/Controllers/CategoryController.cs
@@ -51,10 +51,66 @@ namespace ProductManagement.Controllers
         [HttpGet]
         public IActionResult Details(int id)
         {
-            if(id == 0)
+            if (id == 0)
                 return RedirectToAction("Index", "Category");
             Category category = _db.Categories.FirstOrDefault(x => x.Id == id)!;
+            if (category == null)
+                return NotFound();
             return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id != 0)
+            {
+                Category category = _db.Categories.FirstOrDefault(c => c.Id == id)!;
+                if(category == null)
+                    return NotFound();
+                return View(category);
+            }
+            return RedirectToAction("Index", "Category");
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category == null)
+                return View(category);
+            if (category.Name != null)
+            {
+                // Validate if the category name already exists
+                if (_db.Categories.Any(x => x.Name!.ToLower().Equals(category.Name!.ToLower())))
+                    ModelState.AddModelError("", "This category already exists");
+                if (category.Name!.ToLower().Equals("test") || category.Name!.ToLower().Equals("tests"))
+                    ModelState.AddModelError("", $"{category.Name} is not a valid name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if(id==0)
+                return RedirectToAction("Index", "Category");
+            Category category = _db.Categories.Find(id)!;
+            if (category == null) return NotFound();
+            return View(category);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            if (id == 0) return NotFound();
+            Category category = _db.Categories.Where(c => c.Id.Equals(id)).FirstOrDefault()!;
+            if (category == null) return NotFound();
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Category");
         }
     }
 }
