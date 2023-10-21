41faa69ece1c125a243671867f518bfc979f6d4c
Bernardo Gabriel Mejía Pérez <bernardo.mejiaperez703@gmail.com>
2023-10-21 13:44:04 -0600

    Category Repository Interface

---
+++ ProductManagement_DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using ProductManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagement_DataAccess.Repository.IRepository
+{
+    internal interface ICategoryRepository: IRepository<Category>
+    {
+        void Update(Category obj);
+        void Save();
+    }
+}
