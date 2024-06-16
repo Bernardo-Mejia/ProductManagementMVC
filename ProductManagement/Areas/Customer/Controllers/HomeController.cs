6c552f84805bf1aa3d3b06ee93c310a07b8f9447
Bernardo Gabriel Mejía Pérez <bernardo.mejiaperez703@gmail.com>
2024-06-16 17:44:06 -0600

    Home Page

--- ProductManagement/Areas/Customer/Controllers/HomeController.cs
+++ ProductManagement/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProductManagement.Models;
+using ProductManagement_DataAccess.Repository.IRepository;
 using System.Diagnostics;
 
 namespace ProductManagement.Areas.Customer.Controllers
@@ -8,20 +9,32 @@ namespace ProductManagement.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.ProductRepository.GetAll(includeProperties: "Category");
+            return View(productList);
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult Details(int? productId)
+        {
+            if (productId == null || productId == 0)
+                return NotFound();
+            Product product = _unitOfWork.ProductRepository.Get(x => x.Id == productId, includeProperties: "Category");
+            return View(product);
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
